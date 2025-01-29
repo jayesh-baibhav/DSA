5aad2e61c265aeabeed381cea9cf1a754dbee1d3
jayesh-baibhav <baibhav.jayesh@gmail.com>
2025-01-29 22:25:35 +0530

    practice file

---
+++ play_ground.js
@@ -0,0 +1,7 @@
+/*
+    This file has been created with the purpose of providing a hands-on practice environment. 
+    It is intended to be used for learning, experimentation, and improving coding skills. 
+    Any code within this file should be considered as a means for practice.
+*/
+
+console.log("Hello...")
